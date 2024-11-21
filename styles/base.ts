de97ca4731dd4b678ea174419a9bf89dd5390438
chanyangpraise <cychoi710@icloud.com>
2024-11-21 21:10:36 +0900

    styles base.ts file added

---
+++ styles/base.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const BaseStyles = StyleSheet.create({
+  textInput: {
+    borderRadius: 4,
+    padding: 12,
+    paddingVertical: 16,
+    backgroundColor: '#999',
+    color: 'white',
+  },
+  button: {
+    backgroundColor: '#6466E5',
+    padding: 16,
+    alignItems: 'center',
+    borderRadius: 5,
+  }
+})
+
+export default BaseStyles
