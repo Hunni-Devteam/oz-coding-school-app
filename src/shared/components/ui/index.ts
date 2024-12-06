2d6de2f6ccf95f2417f6a3d6c7b7f97a5535cec6
chanyangpraise <cychoi710@icloud.com>
2024-12-06 14:31:00 +0900

    ♻️ Reorganize project structure for better scalability
    Create src directory with features and shared modules

---
+++ src/shared/components/ui/index.ts
@@ -0,0 +1,4 @@
+export * from './ThemedText';
+export * from './ThemedView';
+export * from './ThemeToggle';
+export * from './ExternalLink';
---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,4 @@
+export * from './useColorScheme';
+export * from './useCustomTheme';
+export * from './useThemeColor';
+export * from './useBaseStyles';
